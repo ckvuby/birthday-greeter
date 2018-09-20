cae923296c9655f234c4152713cb115b2339a589
Vu Le <core.safecrlf>
2018-09-20 11:55:37 +0100

    added test for website to have text asking for the user to input name

--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -4,3 +4,10 @@ feature 'it greets the person' do
     expect(page).to have_content 'Hello there!'
   end
 end
+
+feature 'it asks for the a name' do
+  scenario 'it has text which asks the user to input name' do
+    visit('/')
+    expect(page).to have_content "What's your name"
+  end
+end
